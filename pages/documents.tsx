5292cf0a483edb004610de18fc772e37299e6244
kolapo babawale <kolapobabawale@gmail.com>
2022-06-02 14:13:35 +0100

    documents

---
+++ pages/documents.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import DocumentTemplate from '../app/components/templates/Document';
+
+
+const DocumentPage: NextPage = () => {
+    return (
+
+        <DocumentTemplate />
+
+    )
+}
+
+export default DocumentPage
